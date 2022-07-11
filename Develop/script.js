cda2173a7775fa0087916408175128fed90aa082
Connor Allen <m.connor.allen@gmail.com>
2022-07-11 11:28:56 -0400

    created strings based on choices

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,3 @@
-// Define arrays of characters to be used in password
-var capital = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var num = ['0','1','2','3','4','5','6','7','8','9'];
-var special = ['!','#','$','%','&','(',')','*','+','-','/',':',';','<','>','=','?','@','[',']','^','_','`','{','}','|','~'];
-
 function generatePassword() {
   input = window.parseInt(prompt("How many characters would you like your password to contain? (Please select between 8 and 128."));
 
@@ -18,7 +12,41 @@ function generatePassword() {
     var confirmSpec = window.confirm("Would you like your password to include special characters?");
   };
 
-  
+  if (confirmCap) {
+    var capital = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  } else {
+    var capital = [];
+  }
+
+  if (confirmLow) {
+    var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  } else {
+    var lower = [];
+  }
+
+  if (confirmNum) {
+    var num = ['0','1','2','3','4','5','6','7','8','9'];
+  } else {
+    var num = [];
+  }
+
+  if (confirmSpec) {
+    var special = ['!','#','$','%','&','(',')','*','+','-','/',':',';','<','>','=','?','@','[',']','^','_','`','{','}','|','~'];
+  } else {
+    var special = [];
+  }
+
+// convert arrays to string
+var capitalString = capital.join("");
+var lowerString = lower.join("");
+var numString = num.join("");
+var specString = special.join("");
+
+// combine arrays into string to randomize
+var random = (capitalString + lowerString + numString + specString);
+console.log(random);
+
+
 }
 
 
